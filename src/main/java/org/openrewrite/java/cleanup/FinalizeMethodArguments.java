9ca8128ce302a9fa77fa0342a803b60f9c9fcc76
Ioannis Tsiftzis <ioannis.tsiftzis@oddschecker.com>
2023-03-01 11:17:59 +0200

    detect arguments modifiers

--- src/main/java/org/openrewrite/java/cleanup/FinalizeMethodArguments.java
+++ src/main/java/org/openrewrite/java/cleanup/FinalizeMethodArguments.java
@@ -1,17 +1,12 @@
 /**
  * Copyright 2021 the original author or authors.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  * <p>
  * https://www.apache.org/licenses/LICENSE-2.0
  * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
  **/
 package org.openrewrite.java.cleanup;
 
@@ -22,9 +17,10 @@ import org.openrewrite.Recipe;
 import org.openrewrite.SourceFile;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
-import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.J.Empty;
 import org.openrewrite.java.tree.J.MethodDeclaration;
+import org.openrewrite.java.tree.J.Modifier.Type;
+import org.openrewrite.java.tree.J.VariableDeclarations;
 import org.openrewrite.java.tree.JavaSourceFile;
 import org.openrewrite.java.tree.Statement;
 
@@ -50,7 +46,7 @@ public class FinalizeMethodArguments extends Recipe {
             public MethodDeclaration visitMethodDeclaration(MethodDeclaration methodDeclaration, ExecutionContext executionContext) {
                 MethodDeclaration declarations = super.visitMethodDeclaration(methodDeclaration, executionContext);
 
-                if (isEmpty(declarations.getParameters()) || declarations.getParameters().stream().map(Object::toString).allMatch(param -> param.contains("final"))) {
+                if (isEmpty(declarations.getParameters()) || hasFinalModifiers(declarations.getParameters())) {
                     return declarations;
                 }
                 JavaTemplate addsFinalModifier = JavaTemplate.builder(this::getCursor, declarations.getParameters().stream().map(p -> "final " + p.toString()).collect(Collectors.joining(", ")))
@@ -67,6 +63,19 @@ public class FinalizeMethodArguments extends Recipe {
             }
 
         };
+    }
+
+    private boolean hasFinalModifiers(final List<Statement> parameters) {
+        return parameters.stream().allMatch(p -> {
+            if (p instanceof VariableDeclarations) {
+                if (!((VariableDeclarations) p).getModifiers().isEmpty()
+                    && ((VariableDeclarations) p).getModifiers().stream()
+                    .allMatch(m -> m.getType().equals(Type.Final))) {
+                    return true;
+                }
+            }
+            return false;
+        });
     }
 
     private boolean isEmpty(final List<Statement> parameters) {
